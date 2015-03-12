a0e8fe0e75a65387e3937d1d21c3b9130e32f236
VJoss <vlad.dzhos@gmail.com>
2015-03-12 11:30:30 +0200

    minor

--- SharedSRC/events/Event.cs
+++ SharedSRC/events/Event.cs
@@ -138,7 +138,10 @@ namespace AppAnalytics
             foreach (var it in aList)
             {
                 if (first)
+                {
                     dump.Append(it.ToString());
+                    first = false;
+                }
                 else
                     dump.Append("," + it.ToString(CultureInfo.InvariantCulture));
             }
